749af42456071a00f49e3c046dfcf9db48a6b4d4
John Diego <johnoliveirasilva9@gmail.com>
2020-07-11 01:18:02 -0300

    remove the change style

--- JavaScript HTML/js/script.js
+++ JavaScript HTML/js/script.js
@@ -2,23 +2,6 @@
 //   document.getElementById("demo").innerHTML = "Hello JavaScript!";
 // }
 
-// change the style color
-var count = 0
-function myFunction() {
-  if (count == 0) {
-    document.getElementById("demo").style.fontSize = "25px";
-    document.getElementById("demo").style.color = "red";
-    document.getElementById("demo").style.backgroundColor = "yellow";
-    count++
-  }
-  else {
-    document.getElementById("demo").style.fontSize = "medium";
-    document.getElementById("demo").style.color = "black";
-    document.getElementById("demo").style.backgroundColor = "transparent";
-    count--
-  }
-};
-
 function light(sw) {
   var pic;
   if (sw == 0) {
